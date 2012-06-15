6a5cf403864ff9bb9ff65c86e1094ffc8b4654dd
aarrtteemm <z_artem@yahoo.com>
2012-06-16 00:41:56 +0300

    switched to cmockery UT framework from Google

--- test/testmain.c
+++ test/testmain.c
@@ -1,18 +1,39 @@
-#include "libtcgstorage.h"
-#include "utest.h"
-    
+#include "libtcgstorage.h" 
+
+//header to be included before cmockery.h
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <google/cmockery.h>   
+
+
+#include <assert.h>
+
+// If unit testing is enabled override assert with mock_assert().
+#if UNIT_TESTING
+extern void mock_assert(const int result, const char* const expression, 
+                        const char * const file, const int line);
+#undef assert
+#define assert(expression) \
+    mock_assert((int)(expression), #expression, __FILE__, __LINE__);
+#endif // UNIT_TESTING
+   
+ 
 /**
  * \brief Test for the base TCG Host interface functions
  */
-void test_tcgs_host_basic(void)
+void test_tcgs_host_basic_api(void **state)
 {
 	TCGS_InitHost();
 	TCGS_ResetHost();
-	TCGS_DestroyHost();
+	TCGS_DestroyHost();  
+	assert(false);
 }                     
 
-int main(void)
-{                         
-	run_test("Basic TCG Storage Host API test:", test_tcgs_host_basic());
-	run_end();
+
+int main(int argc, char* argv[]) {
+    const UnitTest tests[] = {
+        unit_test(test_tcgs_host_basic_api),
+    };
+    return run_tests(tests);
 }
